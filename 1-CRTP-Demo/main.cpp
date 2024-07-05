d49bc150dee78797b410cfc81812aecc6d1ec269
刘松坡 <spliul@isstech.com>
2024-07-05 18:14:53 +0800

    first commit

---
+++ 1-CRTP-Demo/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+template <typename Derived>
+struct SomeBase {
+    void foo() {
+        for (auto& item : *static_cast<Derived*>(this)) {
+
+        }
+    }
+};
+
+struct MyClass : SomeBase<MyClass> {
+    class iterator {
+        // your iterator define here
+    };
+
+    iterator begin() const {};
+    iterator end() const {};
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
